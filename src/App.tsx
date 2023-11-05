3d7efbc00fa4fdd31ef693bda26b462ec7908e8d
violettomania <danielpolya@protonmail.com>
2023-11-05 15:55:28 +0100

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+
 export default function App() {
   return (
     <main>
-      <div>
-        <p>it works!</p>
-      </div>
+      <section className='container'>
+        <h1>Questions</h1>
+        <article className='question'>
+          <header>
+            <h5>Do I have to allow the use of cookies?</h5>
+            <button className='question-btn'>
+              <AiOutlinePlus />
+            </button>
+          </header>
+        </article>
+      </section>
     </main>
   );
 }
